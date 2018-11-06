6dc98d1db967082e4c5e050b871570935bca08c5
Barry Gitarts <bgitarts@gmail.com>
2018-11-06 12:57:57 -0500

    ensure proper XY coordinate generation from key for contact code

--- app/utils/ecdsa.js
+++ app/utils/ecdsa.js
@@ -2,18 +2,18 @@ import { ec } from 'elliptic';
 
 const EC = new ec('secp256k1');
 
-export const generateXY = pub => {
-  const stripped = pub.slice(2);
-  const key = EC.keyFromPublic(stripped, 'hex');
-  const pubPoint = key.getPublic();
-  const x = '0x' + pubPoint.getX().toString(16);
-  const y = '0x'+ pubPoint.getY().toString(16);
-  return { x, y };
+export function generateXY (pub) {
+  const stripped = pub.slice(2)
+  const key = EC.keyFromPublic(stripped, 'hex')
+  const pubPoint = key.getPublic()
+  const x = '0x' + pubPoint.getX().toString(16, 64)
+  const y = '0x'+ pubPoint.getY().toString(16, 64)
+  return { x, y }
 }
 
-export const keyFromXY = (X, Y) => {
-  const x = Buffer.from(X.substring(2), 'hex');
-  const y = Buffer.from(Y.substring(2), 'hex');
-  const keys = EC.keyFromPublic({ x, y }, 'hex');
-  return `0x${keys.getPublic().encode('hex')}`;
+export function keyFromXY (X, Y) {
+  const x = Buffer.from(X.substring(2), 'hex')
+  const y = Buffer.from(Y.substring(2), 'hex')
+  const keys = EC.keyFromPublic({ x, y }, 'hex')
+  return `0x${keys.getPublic().encode('hex')}`
 }
