3c2ad0aa1ee4bf735c5053029d83b076caa06b9b
Pierrick TURELIER <pturelier@meilleursagents.com>
2018-11-27 23:52:16 +0100

    Add release domain translation

--- app/components/ens/ReleaseDomain.js
+++ app/components/ens/ReleaseDomain.js
@@ -1,3 +1,4 @@
+import lang from 'i18n-js';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -7,37 +8,36 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
-import classNames from "classnames";
+import classNames from 'classnames';
 
 function Transition(props) {
   return <Slide direction="up" {...props} />;
 }
 
-const styles = theme => ({
-    dialog: {
-        textAlign: 'center',
-    },
-    actions: {
-        background: 'rgba(255, 255, 255, 0.8)',
-        margin: 0,
-        borderTop: 'solid 1px #ccc',
-    },
-    button: {
-        margin: '0',
-        fontSize: '17px',
-        color: '#007AFF',
-        width: '50%',
-        borderRight: 'solid 1px #ccc',
-        borderRadius: 0,
-        padding: '15px',
-    }
+const styles = () => ({
+  dialog: {
+    textAlign: 'center',
+  },
+  actions: {
+    background: 'rgba(255, 255, 255, 0.8)',
+    margin: 0,
+    borderTop: 'solid 1px #ccc',
+  },
+  button: {
+    margin: '0',
+    fontSize: '17px',
+    color: '#007AFF',
+    width: '50%',
+    borderRight: 'solid 1px #ccc',
+    borderRadius: 0,
+    padding: '15px',
+  },
 });
-
 
 const ReleaseDomainAlert = ({ classes, open, handleClose }) => (
   <div>
     <Dialog
-        className={classNames(classes.dialog)}
+      className={classNames(classes.dialog)}
       open={open}
       TransitionComponent={Transition}
       keepMounted
@@ -46,19 +46,19 @@ const ReleaseDomainAlert = ({ classes, open, handleClose }) => (
       aria-describedby="alert-dialog-slide-description"
     >
       <DialogTitle id="alert-dialog-slide-title">
-        {"Release domain?"}
+        {lang.t('domain.release.alert.title')}
       </DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-slide-description">
-          Your SNT deposit will be returned and name will be available to other users.
+          {lang.t('domain.release.alert.text')}
         </DialogContentText>
       </DialogContent>
       <DialogActions className={classNames(classes.actions)}>
         <Button onClick={() => handleClose(null)} className={classNames(classes.button)} color="primary">
-          <strong>Cancel</strong>
+          <strong>{lang.t('action.cancel')}</strong>
         </Button>
         <Button onClick={() => handleClose(true)} className={classNames(classes.button)} color="primary">
-          Yes
+          {lang.t('action.yes')}
         </Button>
       </DialogActions>
     </Dialog>
