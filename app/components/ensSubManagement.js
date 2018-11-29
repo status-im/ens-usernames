4477e2f1a24124a6ac96ee532561de6979b71722
Pierrick TURELIER <pturelier@meilleursagents.com>
2018-11-29 21:52:12 +0100

    Add ens sub management translation

--- app/components/ensSubManagement.js
+++ app/components/ensSubManagement.js
@@ -1,9 +1,8 @@
-import EmbarkJS from 'Embark/EmbarkJS';
+import lang from 'i18n-js';
 import ENSRegistry from 'Embark/contracts/ENSRegistry';
 import UsernameRegistrar from 'Embark/contracts/UsernameRegistrar';
 import TestToken from 'Embark/contracts/TestToken';
 import React, { Fragment } from 'react';
-import { Form, FormGroup, FormControl, HelpBlock, Button, ControlLabel } from 'react-bootstrap';
 import AddDomain from './ens/addDomain';
 import MoveDomain from './ens/moveDomain';
 import RegisterSubDomain from './ens/registerSubDomain';
@@ -11,34 +10,27 @@ import TokenPermissions from './standard/TokenPermission';
 import SetupENS from './ens/setupENS';
 import UpdateController from './ens/updateController';
 
-const FieldGroup = ({ id, label, help, ...props }) => (
-  <FormGroup controlId={id}>
-    <ControlLabel>{label}</ControlLabel>
-    <FormControl {...props} />
-    {help && <HelpBlock>{help}</HelpBlock>}
-  </FormGroup>
-)
-
-const ENSSubManagement = props => (
+const ENSSubManagement = () => (
   <Fragment>
-    <h2 style={{ textAlign: 'center' }}>Subdomain Management</h2>
-    <h3>Change Registry Controller</h3>
+    <h2 style={{ textAlign: 'center' }}>{lang.t('sub_domain.management.title')}</h2>
+    <h3>{lang.t('sub_domain.management.change_registry')}</h3>
     <UpdateController />
-    <h3>Activate Registry/Update Registry Price</h3>
+    <h3>{lang.t('sub_domain.management.activate_registry')}</h3>
     <AddDomain />
-    <h3>Move Domain To Another Registry</h3>
+    <h3>{lang.t('sub_domain.management.move_domain')}</h3>
     <MoveDomain />
-    <hr/>
-    <h3>Register Sub-Domain</h3>
+    <hr />
+    <h3>{lang.t('sub_domain.management.register_sub_domain')}</h3>
     <RegisterSubDomain />
-    <hr/>
+    <hr />
     <TokenPermissions
-      symbol='SNT'
+      symbol="SNT"
       spender={UsernameRegistrar._address}
-      methods={TestToken.methods} />
-    <hr/>
+      methods={TestToken.methods}
+    />
+    <hr />
     <SetupENS ENSRegistry={ENSRegistry} />
   </Fragment>
-)
+);
 
 export default ENSSubManagement;
