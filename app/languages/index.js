101cca16fe8426c3fe20edef466d9af3256a116a
Chad <chad@status.im>
2018-12-05 09:49:12 +0100

    enable russian and korean translations

--- app/languages/index.js
+++ app/languages/index.js
@@ -1,9 +1,13 @@
 import en from './en.json';
 import fr from './fr.json';
+import ko from './ko.json';
+import ru from './ru.json';
 
 const translations = {
   en,
   fr,
+  ko,
+  ru,
 };
 
 export default translations;
