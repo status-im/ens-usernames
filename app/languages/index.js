02d45c468f8ab026d7df58da6f6e4c048855e89d
Chad <chad@status.im>
2018-12-07 07:29:13 +0100

    enable pt_BR language

--- app/languages/index.js
+++ app/languages/index.js
@@ -2,12 +2,14 @@ import en from './en.json';
 import fr from './fr.json';
 import ko from './ko.json';
 import ru from './ru.json';
+import pt_BR from './pt_BR.json';
 
 const translations = {
   en,
   fr,
   ko,
   ru,
+  pt_BR,
 };
 
 export default translations;
